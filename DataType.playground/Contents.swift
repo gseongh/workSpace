bd355eb6c3a416d793a562e0ea6e8505f85ff6be
NatureH <hgs@kakao.com>
2021-12-27 22:35:37 +0900

    DataType 실습

---
+++ DataType.playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+
+var isChecked: Bool = false
+isChecked = true
+
+var temperature: Int = -100
+temperature = 100
+
+var cakes: UInt = 100
+
+var pi: Float = 3.14
+pi = 314
+
+var pi2: Double = 3.14
+pi2 = 314
+
+var sampleCharacter: Character = "A"
+sampleCharacter = "가"
+
+//
+//var sampleString: String = "Hi"
+//sampleString = "안녕하세요"
+
+
+var test = 100
+type(of: test)
+
+var testString = "타입추론"
+type(of: testString)
+
+
+var sampleAny: Any = "any"
+sampleAny = 100000
+sampleAny = 3.14
+
+
+var sampleInt: Int? = nil
+type(of: sampleInt)
+
+
+var sampleString: String? = nil
+type(of: sampleString)
